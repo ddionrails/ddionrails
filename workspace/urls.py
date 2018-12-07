1d8d7517d56842fe2a5e20a738158e642c6c425f
afuetterer <fuetterh@posteo.de>
2018-12-07 18:21:27 +0100

    Migrate workspace urlconf

--- workspace/urls.py
+++ workspace/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 
 from .views import (
@@ -25,51 +25,45 @@ from .views import (
 app_name = "workspace"
 
 urlpatterns = [
-    url(r"^login/$", LoginView.as_view(redirect_authenticated_user=True), name="login"),
-    url(r"^logout/$", LogoutView.as_view(), {"next_page": "/"}),
-    url(r"^register/$", register, name="register"),
-    url(r"^delete_account/$", user_delete),
-    url(r"^password_reset/$", PasswordResetView.as_view()),
-    url(r"^account/$", account_overview, name="account_overview"),
-    url(r"^baskets/$", basket_list, name="basket_list"),
-    url(r"^baskets/new$", basket_new, name="basket_list"),
-    url(r"^baskets/(?P<basket_id>[0-9]+)/csv$", basket_to_csv),
-    url(r"^baskets/(?P<basket_id>[0-9]+)/search$", basket_search),
-    url(r"^baskets/(?P<basket_id>[0-9]+)/delete$", basket_delete),
-    url(
-        r"^baskets/(?P<basket_id>[0-9]+)/add/(?P<variable_id>[0-9]+)/$",
+    path("login/", LoginView.as_view(redirect_authenticated_user=True), name="login"),
+    path("logout/", LogoutView.as_view(), {"next_page": "/"}),
+    path("register/", register, name="register"),
+    path("delete_account/", user_delete),
+    path("password_reset/", PasswordResetView.as_view()),
+    path("account/", account_overview, name="account_overview"),
+    path("baskets/", basket_list, name="basket_list"),
+    path("baskets/new", basket_new, name="basket_list"),
+    path("baskets/<int:basket_id>/csv", basket_to_csv),
+    path("baskets/<int:basket_id>/search", basket_search),
+    path("baskets/<int:basket_id>/delete", basket_delete),
+    path(
+        "baskets/<int:basket_id>/add/<int:variable_id>/",
         add_variable,
         name="add_variable",
     ),
-    url(
-        r"^baskets/(?P<basket_id>[0-9]+)/remove/(?P<variable_id>[0-9]+)/$",
+    path(
+        "baskets/<int:basket_id>/remove/<int:variable_id>/",
         remove_variable,
         name="remove_variable",
     ),
-    url(
-        r"^baskets/(?P<basket_id>[0-9]+)/add_concept/(?P<concept_id>[0-9]+)/$",
+    path(
+        "baskets/<int:basket_id>/add_concept/<int:concept_id>/",
         add_concept,
         name="add_concept",
     ),
-    url(
-        r"^baskets/(?P<basket_id>[0-9]+)/remove_concept/(?P<concept_id>[0-9]+)/$",
+    path(
+        "baskets/<int:basket_id>/remove_concept/<int:concept_id>/",
         remove_concept,
         name="remove_concept",
     ),
-    url(r"^baskets/(?P<basket_id>[0-9]+)$", basket_detail, name="basket"),
-    url(
-        r"^baskets/(?P<basket_id>[0-9]+)/scripts/new/(?P<generator_name>[0-9a-z_-]+)$",
-        script_new_lang,
-    ),
-    url(r"^baskets/(?P<basket_id>[0-9]+)/scripts/new$", script_new),
-    url(
-        r"^baskets/(?P<basket_id>[0-9]+)/scripts/(?P<script_id>[0-9]+)/delete$",
-        script_delete,
-    ),
-    url(
-        r"^baskets/(?P<basket_id>[0-9]+)/scripts/(?P<script_id>[0-9]+)$",
+    path("baskets/<int:basket_id>", basket_detail, name="basket"),
+    path("baskets/<int:basket_id>/scripts/new/<slug:generator_name>", script_new_lang),
+    path("baskets/<int:basket_id>/scripts/new", script_new),
+    path("baskets/<int:basket_id>/scripts/<int:script_id>/delete", script_delete),
+    path(
+        "baskets/<int:basket_id>/scripts/<int:script_id>",
         script_detail,
         name="script_detail",
     ),
-    url(r"^baskets/(?P<basket_id>[0-9]+)/scripts/(?P<script_id>[0-9]+)/raw$", script_raw),
+    path("baskets/<int:basket_id>/scripts/<int:script_id>/raw", script_raw),
 ]
