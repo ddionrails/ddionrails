e8dfebae081d96608d6af125501f8228ff2ed9a6
Marius Jürgen Felix Pahl <marius.jf.pahl@campus.tu-berlin.de>
2018-12-07 15:12:21 +0100

    isort, black and flake8 over two files

--- tests/workspace/test_scripts.py
+++ tests/workspace/test_scripts.py
@@ -1,39 +1,45 @@
 import pytest
 
-from workspace.scripts import SoepStata
-from workspace.scripts import SoepSpss
-from workspace.scripts import SoepR
+from workspace.scripts import SoepR, SoepSpss, SoepStata
 
-### Stata Settings
+# Stata Settings
+
 
 @pytest.fixture
 def soepstata(script, basket):
     return SoepStata(script, basket)
 
+
 @pytest.fixture
 def heading_stata():
     return "* * * GENDER ( male = 1 / female = 2) * * *"
-    
-### SPSS Settings
+
+
+# SPSS Settings
+
 
 @pytest.fixture
 def soepspss(script, basket):
     return SoepSpss(script, basket)
 
+
 @pytest.fixture
 def heading_spss():
     return "### GENDER ( male = 1 / female = 2) ###"
 
-### R Settings
+
+# R Settings
+
 
 @pytest.fixture
 def soepr(script, basket):
     return SoepR(script, basket)
 
+
 @pytest.fixture
 def heading_r():
     return "### GENDER ( male = 1 / female = 2) ###"
-    
+
 
 class TestSoepStataClass:
     def test_render_gender_method_with_male(self, soepstata, heading_stata):
@@ -56,7 +62,8 @@ class TestSoepStataClass:
         command = "\n/* all genders */"
         assert heading_stata in result
         assert command in result
-        
+
+
 class TestSoepSpssClass:
     def test_render_gender_method_with_male(self, soepspss, heading_spss):
         soepspss.settings["gender"] = "m"
@@ -78,7 +85,8 @@ class TestSoepSpssClass:
         command = "\n* all genders *."
         assert heading_spss in result
         assert command in result
-        
+
+
 class TestSoepRClass:
     def test_render_gender_method_with_male(self, soepr, heading_r):
         soepr.settings["gender"] = "m"
