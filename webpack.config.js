b850f307b7955e5cea1ef61a3f9f74d74812d6c1
afuetterer <fuetterh@posteo.de>
2018-12-20 10:42:35 +0100

    Initial commit from ddionrails2/mstats

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,15 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
       }
     ]
   },
