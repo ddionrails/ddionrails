68862ba9e4b53d7af94a85cec4319eeab6ef91ac
afuetterer <fuetterh@posteo.de>
2019-01-16 11:51:01 +0100

    Change if statement in ddionrails/urls.py
    The django debug toolbar and silk apps are running in development settings
    but not on staging. It is possible to use DEBUG = True on staging server
    without installing debug toolbar and silk.

--- ddionrails/urls.py
+++ ddionrails/urls.py
@@ -67,8 +67,7 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
+if settings.DEBUG and ("_hewing" or "_production") not in settings.WSGI_APPLICATION:
     import debug_toolbar
-
     urlpatterns = [path(r"__debug__/", include(debug_toolbar.urls))] + urlpatterns
     urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
