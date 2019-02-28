a79612f0955ed0a8f29a88278bbe3f6bfbd23e0e
afuetterer <fuetterh@posteo.de>
2019-02-28 15:13:13 +0100

    Update instruments admin

--- instruments/admin.py
+++ instruments/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Instrument
 
-admin.site.register(Instrument)
+
+@admin.register(Instrument)
+class InstrumentAdmin(admin.ModelAdmin):
+    list_display = ("name", "study", "period", "analysis_unit")
+    list_filter = ("study", "period", "analysis_unit")
+    list_per_page = 25
+    search_fields = ("name",)
