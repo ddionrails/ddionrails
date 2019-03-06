f25e76d7fb1ed1cfd77d817d89a0ded341c2c64e
afuetterer <fuetterh@posteo.de>
2019-03-06 09:23:40 +0100

    Add workspace tests

--- tests/workspace/test_views.py
+++ tests/workspace/test_views.py
@@ -1,8 +1,26 @@
-from workspace.views import script_detail
+from django.urls import reverse
+
+from workspace.views import script_detail, own_basket_only
+
+from .factories import UserFactory
+
+
+class TestOwnBasketOnlyDecorator:
+    def test_basket_belongs_to_user(self, mocker, basket):
+        pass
+
+    def test_basket_belongs_to_other_user(self, client, basket):
+        other_user = UserFactory(username="other-user")
+        pass
+
+    def test_basket_does_not_exist(self, client, basket):
+        pass
 
 
 class TestScriptDetailView:
-    def test_script_detail_view_with_script_created_before_update(self, rf, basket, script):
+    def test_script_detail_view_with_script_created_before_update(
+        self, rf, basket, script
+    ):
         """ This tests a regression that was introduced after updating the settings for scripts
             with a 'gender' option
         """
@@ -18,3 +36,56 @@ class TestScriptDetailView:
         request = rf.get("script_detail", basket_id=basket.id, script_id=script.id)
         response = script_detail(request, basket.id, script.id)
         assert response.status_code == 200
+
+
+class TestAccountOverview:
+    def test_account_overview_anonymous_user(self, client):
+        url = reverse("workspace:account_overview")
+        response = client.get(url)
+        assert response.status_code == 401
+
+    def test_account_overview_authenticated_user(self, client, user):
+        client.login(username="some-user", password="some-password")
+        url = reverse("workspace:account_overview")
+        response = client.get(url)
+        assert response.status_code == 200
+
+
+class TestBasketList:
+    def test_basket_list_anonymous_user(self, db, client):
+        pass
+
+    def test_basket_list_authenticated_user(self):
+        pass
+
+
+class TestRenderScript:
+    pass
+
+
+class TestAddVariable:
+    pass
+
+
+class TestRemoveVariable:
+    pass
+
+
+class TestAddConcept:
+    pass
+
+
+class TestRemoveConcept:
+    pass
+
+
+class TestBasketCsv:
+    pass
+
+
+class TestBasketDetail:
+    pass
+
+
+class TestRegister:
+    pass
