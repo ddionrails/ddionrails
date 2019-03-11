69fa389ad4c717a2f491f93ba77f95d8143d6ea4
afuetterer <fuetterh@posteo.de>
2019-03-11 18:07:42 +0100

    Add tests

---
+++ tests/api/test_views.py
@@ -0,0 +1,20 @@
+class TestObjectRedirectView:
+    def test_variable_redirect(self, client, variable):
+        url = "/api/test/redirect/variable/1"
+        response = client.get(url)
+        assert 302 == response.status_code
+
+    def test_publication_redirect(self, client, publication):
+        url = "/api/test/redirect/publication/1"
+        response = client.get(url)
+        assert 302 == response.status_code
+
+    def test_question_redirect(self, client, question):
+        url = "/api/test/redirect/question/1"
+        response = client.get(url)
+        assert 302 == response.status_code
+
+    def test_concept_redirect(self, client, concept):
+        url = "/api/test/redirect/concept/1"
+        response = client.get(url)
+        assert 302 == response.status_code
