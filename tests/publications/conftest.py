0ea2d8b9ddbb16e9b4a4b1542b2bf00e04ac1ae1
afuetterer <fuetterh@posteo.de>
2019-03-11 18:07:33 +0100

    Move fixtures

--- tests/publications/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from .factories import PublicationFactory
-
-
-@pytest.fixture
-def publication(study):
-    """ A publication in the database, relates to study fixture """
-    return PublicationFactory(name="some-publication", study=study)
