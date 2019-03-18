a834e739a0446bee83e532dae93e7dc84938750b
afuetterer <fuetterh@posteo.de>
2019-03-18 15:28:27 +0100

    Bugfix
    e.g. "2001".islower() is False

--- ddionrails/validators.py
+++ ddionrails/validators.py
@@ -2,5 +2,5 @@ from django.core.exceptions import ValidationError
 
 
 def validate_lowercase(value):
-    if not value.islower():
+    if not value == value.lower():
         raise ValidationError(f"{value} is not lower case.")
