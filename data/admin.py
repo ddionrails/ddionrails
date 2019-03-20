e9957425984f53c7f9ec9f43b3ec381207957701
afuetterer <fuetterh@posteo.de>
2019-03-20 16:29:40 +0100

    Update VariableAdmin

--- data/admin.py
+++ data/admin.py
@@ -11,4 +11,22 @@ class DatasetAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
-admin.site.register(Variable)
+@admin.register(Variable)
+class VariableAdmin(admin.ModelAdmin):
+    list_display = ("name", "label", "get_dataset_name", "get_study_name")
+    list_filter = ("dataset__study__name",)
+    list_per_page = 25
+    search_fields = ("name", "label")
+    list_select_related = ("dataset", "dataset__study")
+
+    def get_dataset_name(self, obj):
+        return obj.dataset.name
+
+    get_dataset_name.admin_order_field = "dataset"
+    get_dataset_name.short_description = "dataset"
+
+    def get_study_name(self, obj):
+        return obj.dataset.study.name
+
+    get_study_name.admin_order_field = "study"
+    get_study_name.short_description = "study"
