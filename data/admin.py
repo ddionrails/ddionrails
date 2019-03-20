ea9ef174d6a3299504c2e2f621545be76827d3b3
afuetterer <fuetterh@posteo.de>
2019-03-20 17:05:02 +0100

    Update DatasetAdmin

--- data/admin.py
+++ data/admin.py
@@ -5,10 +5,17 @@ from .models import Dataset, Variable
 
 @admin.register(Dataset)
 class DatasetAdmin(admin.ModelAdmin):
-    list_display = ("name", "label", "study", "period", "analysis_unit")
-    list_filter = ("study", "period", "analysis_unit")
+    list_display = ("name", "label", "get_study_name", "period", "analysis_unit")
+    list_filter = ("study__name", "period__name", "analysis_unit__name")
     list_per_page = 25
     search_fields = ("name",)
+    list_select_related = ("study", "period", "analysis_unit")
+
+    def get_study_name(self, obj):
+        return obj.study.name
+
+    get_study_name.admin_order_field = "study"
+    get_study_name.short_description = "study"
 
 
 @admin.register(Variable)
