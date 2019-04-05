7fb5b5f1a7cb6a316e6201ae8087c3fe1a77038b
afuetterer <fuetterh@posteo.de>
2019-04-05 14:31:48 +0200

    Update concepts/admin.py

--- concepts/admin.py
+++ concepts/admin.py
@@ -3,14 +3,24 @@ from django.contrib import admin
 from .models import AnalysisUnit, Concept, ConceptualDataset, Period
 
 
+@admin.register(AnalysisUnit)
+class AnalysisUnitAdmin(admin.ModelAdmin):
+    list_display = ("name", "label")
+
+
+@admin.register(Concept)
+class ConceptAdmin(admin.ModelAdmin):
+    list_display = ("name", "label")
+
+
+@admin.register(ConceptualDataset)
+class ConceptualDatasetAdmin(admin.ModelAdmin):
+    list_display = ("name", "label")
+
+
 @admin.register(Period)
 class PeriodAdmin(admin.ModelAdmin):
     list_display = ("name", "study")
     list_filter = ("study",)
     list_per_page = 25
     search_fields = ("name",)
-
-
-admin.site.register(Concept)
-admin.site.register(AnalysisUnit)
-admin.site.register(ConceptualDataset)
