8d7c2374c4e24a839732f6146f0f027782b70e5f
afuetterer <fuetterh@posteo.de>
2019-04-05 13:44:18 +0200

    Update publications/admin.py

--- publications/admin.py
+++ publications/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 
-from .models import Publication
+from .models import Publication, Attachment
+
+
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ("study", "dataset", "variable", "instrument", "question")
+    list_per_page = 25
+
+    def get_queryset(self, request):
+        queryset = (
+            super()
+            .get_queryset(request)
+            .select_related("study", "dataset", "variable", "instrument", "question")
+        )
+        return queryset
 
 
 @admin.register(Publication)
 class PublicationAdmin(admin.ModelAdmin):
     list_display = ("name", "title", "author", "study", "year")
+    list_select_related = ("study",)
     list_per_page = 25
     search_fields = ("name", "title", "author")
