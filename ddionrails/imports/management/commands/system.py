dfcdca3cb36d5baf1deb8168766341fafc3644b2
afuetterer <fuetterh@posteo.de>
2019-04-11 12:41:14 +0200

    Set up "import_system" management command (#405)
    call python manage.py system

---
+++ ddionrails/imports/management/commands/system.py
@@ -0,0 +1,15 @@
+import djclick as click
+
+from ddionrails.base.models import System
+from ddionrails.imports.manager import Repository, SystemImportManager
+
+@click.command()
+def command():
+    """ Import system settings from GitHub
+
+        call: python manage.py system
+    """
+    system = System.get()
+    repo = Repository(system)
+    repo.update_or_clone_repo()
+    SystemImportManager(system).run_import(import_all=True)
