32b23b340b62c5fca8424d39fbf8d4af64bb2396
hansendx <dhansen@diw.de>
2019-04-15 16:33:37 +0200

    [Docker] Add Nginx and redis
    - Add services nginx and redis
    - Move docker related files to dedicated dir

--- config/settings/production.py
+++ config/settings/production.py
@@ -27,7 +27,7 @@ RQ_QUEUES = {
     "default": {"HOST": "localhost", "PORT": 6379, "DB": 0, "DEFAULT_TIMEOUT": 360},
     "high": {
         "URL": os.getenv(
-            "REDISTOGO_URL", "redis://localhost:6379/0"
+            "REDISTOGO_URL", "redis://redis:6379/0"
         ),  # If you're on Heroku
         "DEFAULT_TIMEOUT": 500,
     },
