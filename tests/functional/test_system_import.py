c090f938daab0ef985489f92397da60bff68e9ab
afuetterer <fuetterh@posteo.de>
2019-04-26 13:58:02 +0200

    Fix broken test case after removing scripts/ directory

--- tests/functional/test_system_import.py
+++ tests/functional/test_system_import.py
@@ -1,33 +1,35 @@
 from pathlib import Path
 
 import pytest
+from click.testing import CliRunner
 
 from ddionrails.base.models import System
+from ddionrails.imports.management.commands import system
 from ddionrails.studies.models import Study
-from scripts import system
 
 pytestmark = [pytest.mark.imports, pytest.mark.functional]
 
 
 class TestSystemImport:
     def test_import_system(self, db):
-        """ Tests the functionality of the script
-            'scripts.system'
+        """ Tests the functionality of the management command 'system'
 
-            it is run by:
-                'paver import_system'
-            or
-                'python manage.py runscript scripts.system'
+            it is run via:
+                'python manage.py system'
 
             This script should:
              - download the system settings from github
              - should import the system settings to the database
              - should import the studies from studies.csv to the database
         """
-        assert Study.objects.count() == 0
-        assert System.objects.count() == 0
-        system.run()
+        assert 0 == Study.objects.count()
+        assert 0 == System.objects.count()
+
+        clirunner = CliRunner()
+        result = clirunner.invoke(system.command)
+        assert result.exit_code == 0
+
         path = Path("static/repositories/system")
         assert path.exists()
-        assert System.objects.count() == 1
-        assert Study.objects.count() == 1
+        assert 1 == Study.objects.count()
+        assert 1 == System.objects.count()
