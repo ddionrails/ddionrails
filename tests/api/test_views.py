c264b9eab3f2b491403854cde5d90d85844284de
afuetterer <fuetterh@posteo.de>
2019-05-17 15:07:03 +0200

    Fix tests for api app that break when run twice (#445)
    The hardcoded ones caused breaking test cases when the tests ran twice.
    
    Fix: Replace the 1 with the actual ID of the object in the database, which
    can be > 1.

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -1,21 +1,24 @@
+# -*- coding: utf-8 -*-
+""" Test cases for ddionrails.api app """
+
 class TestObjectRedirectView:
     def test_variable_redirect(self, client, variable):
-        url = "/api/test/redirect/variable/1"
+        url = f"/api/test/redirect/variable/{variable.id}"
         response = client.get(url)
         assert 302 == response.status_code
 
     def test_publication_redirect(self, client, publication):
-        url = "/api/test/redirect/publication/1"
+        url = f"/api/test/redirect/publication/{publication.id}"
         response = client.get(url)
         assert 302 == response.status_code
 
     def test_question_redirect(self, client, question):
-        url = "/api/test/redirect/question/1"
+        url = f"/api/test/redirect/question/{question.id}"
         response = client.get(url)
         assert 302 == response.status_code
 
     def test_concept_redirect(self, client, concept):
-        url = "/api/test/redirect/concept/1"
+        url = f"/api/test/redirect/concept/{concept.id}"
         response = client.get(url, follow=True)
         assert 200 == response.status_code
         assert response.redirect_chain[-1][0] == concept.get_absolute_url()
