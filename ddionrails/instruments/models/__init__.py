bf54df7a71ac94738922552967ce5dbbcc20b6f6
afuetterer <fuetterh@posteo.de>
2019-05-28 10:02:11 +0200

    [close #507] Update models in instruments app
    - split up long models.py into several modules
    - add help_text to all fields
    - change order of attributes and methods

---
+++ ddionrails/instruments/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+""" Model definitions for ddionrails.instruments app """
+
+from .concept_question import ConceptQuestion
+from .instrument import Instrument
+from .question import Question
+from .question_variable import QuestionVariable
