9b5adfd592aaab7200eeaf92786905a74d72e203
hansendx <dhansen@diw.de>
2019-06-03 13:36:55 +0200

    [ddionrails] Refactor with black and isort

--- tests/publications/test_admin.py
+++ tests/publications/test_admin.py
@@ -8,6 +8,7 @@ from tests import status
 
 pytestmark = [pytest.mark.django_db]
 
+
 def test_attachment_admin_list(admin_client, attachment):
     """ Test the AttachmentAdmin changelist """
     url = reverse("admin:publications_attachment_changelist")
@@ -20,6 +21,7 @@ def test_attachment_admin_detail(admin_client, attachment):
     url = reverse("admin:publications_attachment_change", args=(attachment.id,))
     response = admin_client.get(url)
     assert status.HTTP_200_OK == response.status_code
+
 
 def test_publication_admin_list(admin_client, publication):
     """ Test the PublicationAdmin changelist """
