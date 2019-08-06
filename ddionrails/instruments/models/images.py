5b3f1cb27235c22b237725c4618fcbce08059596
hansendx <dhansen@diw.de>
2019-08-06 09:45:57 +0000

    ?DOCS? Refactor doc comment

--- ddionrails/instruments/models/images.py
+++ ddionrails/instruments/models/images.py
@@ -1,33 +1,16 @@
-# -*- coding: utf-8 -*-
-# This file is part of DDI on Rails.
-#
-# DDI on Rails is free software: you can redistribute it and/or modify
-# it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# DDI on Rails is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU AFFERO GENERAL PUBLIC LICENSE for more details.
-#
-# You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
-# along with DDI on Rails.  If not, see <https://www.gnu.org/licenses/>.
-#
-#
-# - version   : >=3.2.0 (AGPLv3 License)
-# - authors   :
-#   - 2019 Dominique Hansen (DIW Berlin)
-# - date      : 2019-06-11
-# - copyright : Copyright (c) 2019, Dominique Hansen,
-#               DIW Berlin <https://www.diw.de/>. All rights reserved.
-# - license   : AGPL-3.0 GNU AFFERO GENERAL PUBLIC LICENSE (AGPL) 3.0.
-#               See LICENSE at
-#               <https://github.com/ddionrails/ddionrails/blob/master/LICENSE.md>.
-#               See AGPL License at
-#               <https://www.gnu.org/licenses/agpl-3.0.txt>.
+""" Implements a django model to store images for instruments.Question Objects
 
-""" Implements a model to store images for instruments.Question Objects"""
+
+Authors:
+    * 2019 Dominique Hansen (DIW Berlin)
+
+License:
+    | **AGPL-3.0 GNU AFFERO GENERAL PUBLIC LICENSE (AGPL) 3.0**.
+    | See LICENSE at the GitHub
+      `repository <https://github.com/ddionrails/ddionrails/blob/master/LICENSE.md>`_
+    | or at
+      `<https://www.gnu.org/licenses/agpl-3.0.txt>`_.
+"""
 
 import logging
 import uuid
@@ -42,10 +25,9 @@ LOGGER = logging.getLogger("django")
 
 
 class QuestionImage(models.Model):
-    """
-    The Image module is intended to store images for the display on question pages.
+    """The QuestionImage model stores images to be displayed on question pages.
 
-    Images are screnshots of questions.
+    Images are screnshots of questions as seen by study participants.
     They are handled by django-filer and are linked here to a question via a foreign key.
     A question is linked to as many images as there are supported languages.
 
