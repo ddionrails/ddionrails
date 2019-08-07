b5def47c88c1825265fef2cdc17528e173eba0d1
afuetterer <fuetterh@posteo.de>
2019-08-07 12:59:01 +0200

    Forms, imports

--- tests/concepts/test_imports.py
+++ tests/concepts/test_imports.py
@@ -5,14 +5,7 @@
 
 import pytest
 
-from ddionrails.concepts.imports import (
-    AnalysisUnitImport,
-    ConceptImport,
-    ConceptualDatasetImport,
-    PeriodImport,
-    TopicJsonImport,
-)
-from ddionrails.concepts.models import AnalysisUnit, Concept, ConceptualDataset, Period
+from ddionrails.concepts.imports import TopicJsonImport
 from ddionrails.studies.models import TopicList
 
 pytestmark = [pytest.mark.concepts, pytest.mark.imports]  # pylint: disable=invalid-name
@@ -24,87 +17,9 @@ def filename():
 
 
 @pytest.fixture
-def concept_importer(db, filename):  # pylint: disable=unused-argument
-    """ A concept importer """
-    return ConceptImport(filename)
-
-
-@pytest.fixture
-def analysis_unit_importer(db, filename, study):  # pylint: disable=unused-argument
-    """ An analysis unit importer """
-    return AnalysisUnitImport(filename, study)
-
-
-@pytest.fixture
-def conceptual_dataset_importer(db, filename, study):  # pylint: disable=unused-argument
-    """ A conceptual dataset importer """
-    return ConceptualDatasetImport(filename, study)
-
-
-@pytest.fixture
-def period_importer(db, filename, study):  # pylint: disable=unused-argument
-    """ A period importer """
-    return PeriodImport(filename, study)
-
-
-@pytest.fixture
 def topic_json_importer(db, filename, study):  # pylint: disable=unused-argument
     """ A topic json importer """
     return TopicJsonImport(filename, study)
-
-
-class TestConceptImport:
-    def test_import_with_valid_data(self, concept_importer, valid_concept_data):
-        response = concept_importer.import_element(valid_concept_data)
-        assert isinstance(response, Concept)
-        assert response.name == valid_concept_data["concept_name"]
-
-    def test_import_with_invalid_data(self, concept_importer, empty_data):
-        response = concept_importer.import_element(empty_data)
-        expected = None
-        assert expected is response
-
-
-class TestAnalysisUnitImport:
-    def test_import_with_valid_data(
-        self, analysis_unit_importer, valid_analysis_unit_data
-    ):
-        response = analysis_unit_importer.import_element(valid_analysis_unit_data)
-        assert isinstance(response, AnalysisUnit)
-        assert response.name == valid_analysis_unit_data["analysis_unit_name"]
-
-    def test_import_with_invalid_data(self, analysis_unit_importer, empty_data):
-        response = analysis_unit_importer.import_element(empty_data)
-        expected = None
-        assert expected is response
-
-
-class TestConceptualDatasetImport:
-    def test_import_with_valid_data(
-        self, conceptual_dataset_importer, valid_conceptual_dataset_data
-    ):
-        response = conceptual_dataset_importer.import_element(
-            valid_conceptual_dataset_data
-        )
-        assert isinstance(response, ConceptualDataset)
-        assert response.name == valid_conceptual_dataset_data["conceptual_dataset_name"]
-
-    def test_import_with_invalid_data(self, conceptual_dataset_importer, empty_data):
-        response = conceptual_dataset_importer.import_element(empty_data)
-        expected = None
-        assert expected is response
-
-
-class TestPeriodImport:
-    def test_import_with_valid_data(self, period_importer, valid_period_data):
-        response = period_importer.import_element(valid_period_data)
-        assert isinstance(response, Period)
-        assert response.name == valid_period_data["period_name"]
-
-    def test_import_with_invalid_data(self, period_importer, empty_data):
-        response = period_importer.import_element(empty_data)
-        expected = None
-        assert expected is response
 
 
 class TestTopicJsonImport:
--- tests/data/test_imports.py
+++ tests/data/test_imports.py
@@ -6,80 +6,15 @@
 import pytest
 
 from ddionrails.concepts.models import AnalysisUnit, ConceptualDataset, Period
-from ddionrails.data.imports import DatasetImport, DatasetJsonImport, VariableImport
+from ddionrails.data.imports import DatasetJsonImport
 from ddionrails.data.models import Dataset, Variable
 
 pytestmark = [pytest.mark.data, pytest.mark.imports]
 
 
 @pytest.fixture
-def dataset_csv_importer(study):
-    return DatasetImport("DUMMY.csv", study)
-
-
-@pytest.fixture
 def dataset_json_importer(study):
     return DatasetJsonImport("DUMMY.csv", study)
-
-
-@pytest.fixture
-def variable_importer(study):
-    return VariableImport("DUMMY.csv", study)
-
-
-class TestDatasetImport:
-    """ Tests for csv based dataset imports """
-
-    @pytest.mark.django_db
-    def test__import_dataset_links_method_gets_called(self, mocker, dataset_csv_importer):
-        valid_dataset_data = dict(dataset_name="some-dataset")
-        mocker.patch("ddionrails.data.imports.DatasetImport._import_dataset_links")
-        dataset_csv_importer.import_element(valid_dataset_data)
-        DatasetImport._import_dataset_links.assert_called_once()
-
-    def test__import_dataset_links_method_with_minimal_fields(
-        self, dataset, mocker, dataset_csv_importer
-    ):
-        """ This import method needs an already existing dataset and study in the database """
-        valid_dataset_data = dict(dataset_name="some-dataset")
-        assert 1 == Dataset.objects.count()
-        dataset_csv_importer._import_dataset_links(
-            valid_dataset_data
-        )  # pylint: disable=protected-access
-        assert 1 == Dataset.objects.count()
-        dataset = Dataset.objects.get(name=valid_dataset_data["dataset_name"])
-
-        analysis_unit = AnalysisUnit.objects.get(name="none")
-        conceptual_dataset = ConceptualDataset.objects.get(name="none")
-        period = Period.objects.get(name="none")
-
-        # check relations are set correctly
-        assert dataset.analysis_unit == analysis_unit
-        assert dataset.conceptual_dataset == conceptual_dataset
-        assert dataset.period == period
-
-    def test__import_dataset_links_method_with_more_fields(
-        self, dataset, dataset_csv_importer
-    ):
-        """ This import method needs an already existing dataset and study in the database """
-
-        valid_dataset_data = dict(
-            dataset_name="some-dataset",
-            label="Some dataset",
-            description="This is some dataset",
-            analysis_unit_name="some-analysis-unit",
-            conceptual_dataset_name="some-conceptual-dataset-name",
-            period_name="some-period-name",
-        )
-
-        assert Dataset.objects.count() == 1
-        dataset_csv_importer._import_dataset_links(valid_dataset_data)
-        assert Dataset.objects.count() == 1
-        dataset = Dataset.objects.get(name=valid_dataset_data["dataset_name"])
-
-        assert dataset.analysis_unit.name == "some-analysis-unit"
-        assert dataset.conceptual_dataset.name == "some-conceptual-dataset-name"
-        assert dataset.period.name == "some-period-name"
 
 
 class TestDatasetJsonImport:
@@ -213,44 +148,3 @@ class TestDatasetJsonImport:
         dataset_json_importer._import_variable(
             var, dataset, sort_id
         )  # pylint: disable=protected-access
-
-
-class TestVariableImport:
-    def test_import_element_method(self, mocker, variable_importer, dataset):
-        mocked_import_variable_links = mocker.patch.object(
-            VariableImport, "_import_variable_links"
-        )
-        element = dict(dataset_name=dataset.name, variable_name="some-variable")
-        variable_importer.import_element(element)
-        mocked_import_variable_links.assert_called_once()
-
-    def test_import_element_method_fails(
-        self, mocker, capsys, variable_importer, dataset
-    ):  # pylint: disable=unused-argument
-        mocked_import_variable_links = mocker.patch.object(
-            VariableImport, "_import_variable_links"
-        )
-        mocked_import_variable_links.side_effect = KeyError
-        element = dict(dataset_name="asdas", variable_name="")
-        variable_importer.import_element(element)
-        mocked_import_variable_links.assert_called_once()
-
-    def test_import_variable_links_method(self, variable_importer, variable):
-        element = dict(dataset_name=variable.dataset.name, variable_name=variable.name)
-        variable_importer._import_variable_links(
-            element
-        )  # pylint: disable=protected-access
-
-    def test_import_variable_links_method_with_concept_name(
-        self, variable_importer, variable
-    ):
-        element = dict(
-            dataset_name=variable.dataset.name,
-            variable_name=variable.name,
-            concept_name="some-concept",
-            description="some-description",
-        )
-        variable_importer._import_variable_links(element)
-        variable = Variable.objects.get(id=variable.id)
-        assert variable.description == element["description"]
-        assert variable.concept.name == element["concept_name"]
