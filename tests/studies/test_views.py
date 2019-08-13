38afd99d19332cd95d06c843c63592d400fa3bf0
afuetterer <fuetterh@posteo.de>
2019-08-13 13:14:30 +0200

    [close #725] Remove StudyRedirectView

--- tests/studies/test_views.py
+++ tests/studies/test_views.py
@@ -4,28 +4,13 @@
 """ Test cases for views in ddionrails.studies app """
 
 import pytest
-from django.http.response import Http404
 from django.urls import reverse
 
-from ddionrails.studies.views import StudyDetailView, StudyRedirectView
 from tests import status
 
 pytestmark = [pytest.mark.studies, pytest.mark.views]
 
-
-class TestStudyRedirectView:
-    def test_study_redirect(self, rf, study):
-        request = rf.get("study", kwargs={"pk": study.pk})
-        response = StudyRedirectView.as_view()(request, id=study.pk)
-
-        assert status.HTTP_302_FOUND == response.status_code
-
-    @pytest.mark.django_db
-    def test_study_redirect_invalid_id(self, rf):
-        invalid_study_id = 999
-        request = rf.get("study", kwargs={"pk": invalid_study_id})
-        with pytest.raises(Http404):
-            StudyRedirectView.as_view()(request, id=invalid_study_id)
+LANGUAGE = "en"
 
 
 class TestStudyDetailView:
@@ -44,24 +29,27 @@ class TestStudyDetailView:
         assert expected_instruments == output_instruments
 
     @pytest.mark.django_db
-    def test_study_detail_view_with_non_existing_study_name(self, rf):
+    def test_study_detail_view_with_non_existing_study_name(self, client):
         non_existing_study_name = "not-in-database"
         url = reverse("study_detail", kwargs={"study_name": non_existing_study_name})
-        request = rf.get(url)
-        with pytest.raises(Http404):
-            StudyDetailView.as_view()(request, study_name=non_existing_study_name)
+        response = client.get(url)
+        assert status.HTTP_404_NOT_FOUND == response.status_code
 
 
-# class TestStudyTopicsView:
-#     def test_study_topics_view_with_existing_study_name(self, rf, study):
-#         url = reverse("study.topics", kwargs={"study_name": study.name})
-#         request = rf.get(url)
-#         response = study_topics(request, study_name=study.name)
-#         assert response.status_code == 200
-#
-#     def test_study_topics_view_with_non_existing_study_name(self, rf, db):
-#         non_existing_study_name = "not-in-database"
-#         url = reverse("study.topics", kwargs={"study_name": non_existing_study_name})
-#         request = rf.get(url)
-#         with pytest.raises(Http404):
-#             study_topics(request, study_name=non_existing_study_name)
+class TestStudyTopicsView:
+    def test_study_topics_view_with_existing_study_name(self, client, study):
+        url = reverse(
+            "study_topics", kwargs={"study_name": study.name, "language": LANGUAGE}
+        )
+        response = client.get(url)
+        assert status.HTTP_200_OK == response.status_code
+
+    @pytest.mark.django_db
+    def test_study_topics_view_with_non_existing_study_name(self, client):
+        non_existing_study_name = "not-in-database"
+        url = reverse(
+            "study_topics",
+            kwargs={"study_name": non_existing_study_name, "language": LANGUAGE},
+        )
+        response = client.get(url)
+        assert status.HTTP_404_NOT_FOUND == response.status_code
