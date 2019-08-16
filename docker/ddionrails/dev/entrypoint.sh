fa715ed3f3a01f5b4e0153259a00892a6a2306d0
hansendx <dhansen@diw.de>
2019-08-16 07:29:02 +0000

    -FIX- Keep static/dist from being empty
    Problem
    -------
    
    * Production image no longer contains npm, as it is removed after
    the webpack build process.
    * entrypoint.sh contains a section to update npm packages.
    
      + This rebuild removed all files build by webpack before rebuilding.
      + Rebuilding could not be done since npm was no longer installed.
      + This left static/dist empty.
    
    Patch
    -----
    
    * entrypoints for development and production are now separate.
    
      + The content of static/dist is now neither rebuild nor removed inside
        the production container.
    
    * Mounting of the static volume is now more verbose/explicit.
    
      + web now mounts it explicitly with nocopy set to false
      + nginx now mounts it explicitly with nocopy set to true
    
    NOTE
    ----
    
    Changes of npm dependencies should only come through newly build images
    to production systems.

---
+++ docker/ddionrails/dev/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Update static files if needed.
+WEBPACK_STATS=webpack-stats.json
+BUILD_DEPENDENCIES=/usr/src/app/${WEBPACK_STATS}
+LIVE_DEPENDENCIES=/usr/src/app/static/${WEBPACK_STATS}
+
+echo "Check for old dependencies"
+cmp "${BUILD_DEPENDENCIES}" "${LIVE_DEPENDENCIES}"
+DEPENDENCY_DIFF=$?
+
+if [ "${DEPENDENCY_DIFF}" -gt 0 ] || [ ! -f "${LIVE_DEPENDENCIES}" ] || [ ! -f "${BUILD_DEPENDENCIES}" ]; then
+    echo "Image dependencies have changed."
+    echo "Overwriting old dependencies."
+    rm -rf /usr/src/app/static/dist/*
+    npm install
+    cd /usr/src/app
+    npm run build
+    cp ${BUILD_DEPENDENCIES} ${LIVE_DEPENDENCIES}
+fi
+
+
+# Collect Admin stiling etc.
+python manage.py collectstatic --noinput
+
+echo "Initialising System"
+python manage.py system || echo "Initialising System failed." &
+
+echo "Running migrations."
+python manage.py migrate && \
+echo "Starting rqworker" && \
+python manage.py rqworker &
+
+echo "Creating search indices"
+python manage.py search_index --create || echo "Creating search indices failed." &
+
+echo "Starting server"
+exec "$@"
+
