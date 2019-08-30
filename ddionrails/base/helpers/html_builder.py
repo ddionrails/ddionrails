7e8c9781cbc7a88499a96fff0826fda96b82f079
hansendx <dhansen@diw.de>
2019-08-30 08:33:24 +0000

    Add function to create table headers
    * Add yettag as dependency to build html
    * Use yettag in build_table_header
    * Change tests to test wit random lower cased text
    * Change unittest pre-commit hook to not write coverage report
    
      + Coverage report is interpreted as file change, causing
        the hook to fail.

--- ddionrails/base/helpers/html_builder.py
+++ ddionrails/base/helpers/html_builder.py
@@ -1,11 +1,28 @@
 """ Build HTML Snippets for Django views. """
-# pylint: disable=all
+
+from yattag import Doc
 
 
-class TableBuilder:
+class TableBuilder:  # pylint: disable=too-few-public-methods
     """ Functions to build HTML Table snippets """
 
     @classmethod
-    def build_table_header():
-        """ Build a table header """
-        pass
+    def build_table_header(cls, header_entries: list) -> str:
+        """ Build a htm table header.
+
+        Args:
+            headers_entries: Text entries to appear in the header
+
+        Returns:
+            str: An html header containing the input list elements as header elements
+        """
+
+        doc, tag, text = Doc().tagtext()
+
+        with tag("tr"):
+            for header in header_entries:
+                with tag("th"):
+                    text(header)
+
+        html = doc.getvalue()
+        return html
--- tests/base/helpers/test_html_builder.py
+++ tests/base/helpers/test_html_builder.py
@@ -1,9 +1,34 @@
-# pylint: disable=missing-docstring
+""" Test functionality related to building html Fragments. """
+import re
 import unittest
+from random import randint
 
 from ddionrails.base.helpers.html_builder import TableBuilder
 
 
 class TestTableBuilder(unittest.TestCase):
+    """ Test Functionality related to table creation. """
+
+    ALPHA_START = 97
+    ALPHA_END = 122
+
+    def _random_alphabetical_string(self):
+        _length = randint(7, 10)  # nosec
+        _string = ""
+        for _ in range(3, _length):
+            _string = _string + chr(randint(self.ALPHA_START, self.ALPHA_END))  # nosec
+        return _string
+
     def test_build_table_header(self):
+        """ Can we get a html table header from a list input """
+        # Does our function exist?
         self.assertTrue(callable(TableBuilder.build_table_header))
+        test_input = [
+            self._random_alphabetical_string(),
+            self._random_alphabetical_string(),
+        ]
+        expected = "<tr><th>{}</th><th>{}</th></tr>".format(*test_input)
+        result = TableBuilder.build_table_header(test_input)
+        # Is The structure as expected?
+        # Not caring about whitespaces here.
+        self.assertEqual(expected, re.sub(r"\s+", "", result))
