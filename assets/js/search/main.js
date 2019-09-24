f830c063642703c17d1cd7fc1d27de54c1b8bb25
hansendx <dhansen@diw.de>
2019-09-24 09:28:05 +0000

    [Close #815] Add observer to change result list
    MutationObserver checks for changes in the result list
    text and adds an "Over " to the start, if the text
    starts with 10000.

--- assets/js/search/main.js
+++ assets/js/search/main.js
@@ -17,6 +17,38 @@ import router from "./router";
 Vue.use(ReactiveSearch);
 Vue.config.productionTip = false;
 
+/**
+ * The result list in of a search contains a p note with text like:
+ * "x results found in yms". Elasticsearch 7 by default only returns
+ * 10,000 as result count if the number of results exceeds 10,000.
+ * To avoid confusion for users, it should be indicated, that the actual number
+ * above 10,000. The following observer is supposed to watch the text node and
+ * add "Over " to the start of it if displays "10000".
+ * The message should display a message like "Over 10000 results found in yms".
+ */
+const resultObserver = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.target.data.includes("10000")) {
+      if (! mutation.target.data.includes("Over")) {
+        mutation.target.data = "Over " + mutation.target.data;
+      }
+    }
+  });
+});
+
+window.onload = function() {
+  // Observe the result text, after page is loaded.
+  resultObserver.observe(document.querySelector("p.css-1e7votj"), {
+    attributes: true,
+    characterData: true,
+    childList: true,
+    subtree: true,
+    attributeOldValue: true,
+    characterDataOldValue: true,
+  });
+};
+// End of observer declaration.
+
 new Vue({
   router,
   render: (h) => h(App),
