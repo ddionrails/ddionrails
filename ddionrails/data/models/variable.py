e56642c590cd6a031cca2f9260b730da946ce737
hansendx <dhansen@diw.de>
2020-01-20 13:06:07 +0100

    Limit LabelTable size (#1052)
    Problem
    -------
    
    A variable with many value labels and/or many
    related variables would cause a server timeout.
    This is caused by the generation of a string
    containing HTML that is rendered by a LabelTable
    instance.
    
    Solution
    --------
    
    Stop the LabelTable from rendering if a variable
    has more than 100 related variables or more than a
    100 value labels.
    
    Related Changes
    ---------------
    
    * labels was added as LabelTable instance property and
      set during __init__.
    
      + Category labels are now only retrieved once.
      + Labels can now be counted before the call to to_html.
      + The count can be used to determine if the LabelTable
        should be printed.
    
    * label_count is now also set during __init__
      with the length of self.labels.
    
    Other Changes
    -------------
    
    * Refactor Variable.get_categories
    
      + labels_de now defaults to the content
        of labels if not provided.
    
    * Remove some lint
    * Add tests for new functionality and to cover more of
      the existing code.

--- ddionrails/data/models/variable.py
+++ ddionrails/data/models/variable.py
@@ -223,15 +223,16 @@ class Variable(ModelMixin, models.Model):
         """ Return a list of dictionaries based on the "categories" field """
         if self.categories:
             categories = []
+            if "labels_de" not in self.categories:
+                self.categories["labels_de"] = self.categories["labels"]
             for index, _ in enumerate(self.categories["values"]):
                 category = dict(
                     value=self.categories["values"][index],
                     label=self.categories["labels"][index],
                     frequency=self.categories["frequencies"][index],
                     valid=(not self.categories["missings"][index]),
+                    label_de=self.categories["labels_de"][index],
                 )
-                if "labels_de" in self.categories:
-                    category["label_de"] = self.categories["labels_de"][index]
                 categories.append(category)
             return categories
 
@@ -267,7 +268,7 @@ class Variable(ModelMixin, models.Model):
         ):
             if self.concept:
                 variables = Variable.objects.select_related(
-                    "dataset", "dataset__study"
+                    "dataset", "dataset__study", "period"
                 ).filter(concept=self.concept, dataset__study=self.dataset.study)
                 self.related_cache = self.Cache(id=self.concept.id, content=variables)
             else:
