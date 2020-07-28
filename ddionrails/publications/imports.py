635f57d21257ea4990b9d995980a9e70b928151e
hansendx <dhansen@diw.de>
2020-07-28 10:33:06 +0000

    Fix attachment import; refactor module
    Import only worked for old csv headers with "_name" prefix.
    
    Removed useless implementation of superclass method in
    PublicationImport class.

--- ddionrails/publications/imports.py
+++ ddionrails/publications/imports.py
@@ -4,10 +4,15 @@
 
 import logging
 from collections import OrderedDict
+from typing import Dict
+
+from django.db.models import Model
+from django.db.transaction import atomic
 
 from ddionrails.data.models import Dataset, Variable
 from ddionrails.imports import imports
 from ddionrails.instruments.models import Instrument, Question
+from ddionrails.publications.models import Attachment
 
 from .forms import AttachmentForm, PublicationForm
 from .models import Publication
@@ -24,9 +29,6 @@ class PublicationImport(imports.CSVImport):
         element["study"] = self.study.id
         return element
 
-    def import_element(self, element: OrderedDict) -> Publication:
-        return super().import_element(element)
-
 
 class AttachmentImport(imports.CSVImport):
     class DOR:  # pylint: disable=missing-docstring,too-few-public-methods
@@ -36,39 +38,80 @@ class AttachmentImport(imports.CSVImport):
         super().__init__(*args, **kwargs)
         self.study.related_attachments.all().delete()
 
-    def process_element(self, element):
-        element["context_study"] = self.study.id
-        t = element.get("type")
-        if t == "study":
-            element["study"] = self.study.id
-        elif t == "variable":
-            element["variable"] = (
-                Variable.objects.filter(
-                    dataset__study_id=self.study.id,
-                    dataset__name=element.get("dataset_name"),
-                )
-                .get(name=element.get("variable_name"))
-                .id
+    @atomic
+    def execute_import(self):
+        for attachment in self.content:
+            attachment = self._process_field_names(attachment)
+            related = self._get_related_object(attachment)
+            attachment = dict(
+                **related, url=attachment["url"], url_text=attachment["url_text"]
             )
-        elif t == "dataset":
-            element["dataset"] = (
-                Dataset.objects.filter(study_id=self.study.id)
-                .get(name=element.get("dataset_name"))
-                .id
-            )
-        elif t == "question":
-            element["question"] = (
-                Question.objects.filter(
-                    instrument__study_id=self.study.id,
-                    instrument__name=element.get("instrument_name"),
-                )
-                .get(name=element.get("question_name"))
-                .id
-            )
-        elif t == "instrument":
-            element["instrument"] = (
-                Instrument.objects.filter(study_id=self.study.id)
-                .get(name=element.get("instrument_name"))
-                .id
-            )
-        return element
+            self.import_element(attachment)
+
+    def import_element(self, element):
+        attachment, _ = Attachment.objects.get_or_create(
+            url=element["url"], context_study=self.study
+        )
+        del element["url"]
+        for key, value in element.items():
+            setattr(attachment, key, value)
+        attachment.save()
+
+    @staticmethod
+    def _process_field_names(dictionairy: Dict[str, str]) -> Dict[str, str]:
+        """Replace outdated field names
+
+        Remove the outdated "_name" prefix from  dictionairy keys.
+
+        Returns:
+            New dictionairy with "_name" removed from keys.
+        """
+        cleaned_dictionairy = dict()
+        for key, value in dictionairy.items():
+            if key[-5:] == "_name":
+                cleaned_dictionairy[key[:-5]] = value
+            else:
+                cleaned_dictionairy[key] = value
+        return cleaned_dictionairy
+
+    def _get_related_object(self, element: Dict[str, str]) -> Dict[str, Model]:
+        """Retrieve the attachement target from the database.
+
+        Arguments:
+            element: Metadata about the target object of the attachment.
+
+        Returns:
+            A single key dictionairy where the key is the field name of the
+            target and the value is the target object.
+        """
+        element["context_study"] = self.study
+        _type = element.get("type")
+        dataset = element.get("dataset")
+        if _type == "study":
+            return {"study": self.study}
+        call_dict = {
+            "variable": (
+                Variable,
+                {
+                    "dataset__study": self.study,
+                    "dataset__name": dataset,
+                    "name": element.get("variable"),
+                },
+            ),
+            "dataset": (Dataset, {"name": element.get("dataset"), "study": self.study}),
+            "question": (
+                Question,
+                {
+                    "instrument__study": self.study,
+                    "instrument__name": element.get("instrument"),
+                    "name": element.get("question"),
+                },
+            ),
+            "instrument": (
+                Instrument,
+                {"study": self.study, "name": element.get("instrument")},
+            ),
+        }
+        model, kwargs = call_dict[_type]
+        _object = model.objects.get(**kwargs)
+        return {_type: _object}
