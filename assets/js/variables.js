ccd54d3e818cfee2da806024082cd4ea8f9d0580
GitHub Action <dhansen@diw.de>
2020-10-29 08:08:53 +0000

    Implement anchor link focussing
    When variable view is entered with an anchor in the URL
    focus on the anchor and open tab content.

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -18,3 +18,20 @@ $(window).on("load", function() {
 }
 
 );
+
+/**
+ * When URL contains an anchor:
+ * Open tab content of the anchor and focus on the content.
+ */
+$(window).on("load", function() {
+  const href = window.location.href;
+  const anchor =href.substr(href.indexOf("#"));
+  if ( anchor.length === 1 ) {
+    return;
+  }
+  const anchorLink = $(`a[href$="${anchor}"]`);
+  anchorLink[0].click();
+  $([document.documentElement, document.body]).animate({
+    scrollTop: anchorLink.offset().top - $("#paneldata-navbar").outerHeight(),
+  }, 2000);
+});
