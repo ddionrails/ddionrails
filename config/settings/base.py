ecd0fd2796d275974c25ed337071aa29de46109d
Dominique Hansen <dhansen@diw.de>
2021-01-13 07:50:23 +0000

    Fix authentication in rest framework for anon user

--- config/settings/base.py
+++ config/settings/base.py
@@ -178,6 +178,9 @@ WEBPACK_LOADER = {
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication"
+    ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ],
