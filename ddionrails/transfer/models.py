9cc4d0a2b32bed875aaaccbb589c670dc449a68e
root <dhansen@diw.de>
2021-09-02 11:23:06 +0000

    Add model for variable statistics

--- ddionrails/transfer/models.py
+++ ddionrails/transfer/models.py
@@ -1,3 +1,24 @@
+"""Models for the transfer-server App."""
 from django.db import models
 
+from ddionrails.data.models.variable import Variable
+
 # Create your models here.
+
+
+class VariableStatistic(models.Model):
+    """Stores CSV statistics Data for a data.Variable."""
+
+    class PlotType(models.TextChoices):
+        """Define possible plot types."""
+
+        CATEGORICAL = "categorical", "Categorical"
+        NUMERICAL = "numerical", "Numerical"
+
+    # Keys
+    id = models.AutoField(primary_key=True)
+    variable = models.ForeignKey(to=Variable, null=False, on_delete=models.CASCADE)
+
+    # Attributes
+    plot_type = models.TextField(null=False, choices=PlotType.choices)
+    statistics = models.TextField(null=False, blank=False)
