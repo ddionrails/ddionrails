c6b9c45d508d2a88cd05f54a418758cd2de4b70f
root <dhansen@diw.de>
2021-09-13 11:38:46 +0000

    Add QuestionItem model

--- ddionrails/instruments/models/__init__.py
+++ ddionrails/instruments/models/__init__.py
@@ -5,4 +5,5 @@ from .concept_question import ConceptQuestion
 from .images import QuestionImage
 from .instrument import Instrument
 from .question import Question
+from .question_item import QuestionItem
 from .question_variable import QuestionVariable
