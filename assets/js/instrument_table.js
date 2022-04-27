19c56ba82983e7d2f7497c86f907f5855ba38f1d
root <dhansen@diw.de>
2022-04-27 09:08:21 +0000

    Implement column search

--- assets/js/instrument_table.js
+++ assets/js/instrument_table.js
@@ -7,15 +7,21 @@ const instrumentApiURL = new URL("api/instruments/", window.location.origin);
 const urlPart = "inst";
 const study = document.querySelector("#study-name").getAttribute("value");
 
+const inputTemplate = document.createElement("input");
+inputTemplate.type = "text";
+inputTemplate.classList.add("form-control", "form-control-sm");
+
 
 /**
  * Renders a table of instruments.
  *
  * @param {Element} table Empty table to be filled through dataTable instantiation.
  * @param {*} url         API URL to call for the metadata of the desired entities.
+ * @return {DataTable}    A dataTable object with full API.
  */
-function renderEntityTable(table, url) {
-  $(table).dataTable(
+function renderInstrumentTable(table, url) {
+  // eslint-disable-next-line new-cap
+  return $(table).DataTable(
     {
       "ajax":
       {
@@ -57,13 +63,50 @@ function renderEntityTable(table, url) {
   );
 };
 
+/**
+ *
+ * @param {*} table    DOM element object of the table.
+ * @param {*} tableAPI dataTable object of the table with full API.
+ * @return {object}    Maps column names to input elements.
+ */
+function addSearchInput(table) {
+  const header = table.querySelectorAll(".header > th");
+  const searchInputs = {};
+  for (const headerColumn of header) {
+    const columnName = headerColumn.getAttribute("title");
+    const searchHead = table.querySelector(`.search-header > .${columnName}`);
+    const searchInput = inputTemplate.cloneNode();
+    searchHead.appendChild(searchInput);
+    searchInputs[columnName.toString()] = searchInput;
+  }
+  return searchInputs;
+}
+
+/**
+ *
+ * @param {*} event
+ * @param {*} columnName
+ * @param {*} tableAPI
+ */
+function addSearchEvent(event) {
+  event.currentTarget.tableAPI.column(`.${event.currentTarget.column}`).search(
+    event.currentTarget.value)
+    .draw();
+}
+
 window.addEventListener("load", function() {
   const instrumentTable = document.querySelector("#instrument-table");
 
   const instrumentAPI = new URL(instrumentApiURL.toString());
   instrumentAPI.searchParams.append("study", study);
   instrumentAPI.searchParams.append("paginate", "False");
-  renderEntityTable(instrumentTable, instrumentAPI);
+  const tableAPI = renderInstrumentTable(instrumentTable, instrumentAPI);
+  const columnInputMapping = addSearchInput(instrumentTable);
+  for (const columnName of Object.keys(columnInputMapping)) {
+    columnInputMapping[columnName.toString()].column = columnName;
+    columnInputMapping[columnName.toString()].tableAPI = tableAPI;
+    columnInputMapping[columnName.toString()].addEventListener( "input", addSearchEvent);
+  }
 });
 
 
