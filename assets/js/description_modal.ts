754d064bf43b2d7c39d007e68ad59a7817a25f3c
root <dhansen@diw.de>
2022-08-08 11:02:57 +0000

    Refactor question modal handling

--- assets/js/description_modal.ts
+++ assets/js/description_modal.ts
@@ -5,19 +5,17 @@
 window.addEventListener("load", () => {
   const description = document.getElementById("description-card-content");
   if (description.textContent.trim() === "") {
-    const parent = description.parentNode as HTMLElement;
-    parent.classList.add("hidden");
     return;
   }
-  if (
-    description.scrollHeight - description.clientHeight <= 1
-  ) {
+
+  const parent = description.parentNode as HTMLElement;
+  parent.classList.remove("hidden");
+  if (description.scrollHeight - description.clientHeight <= 1) {
     document.getElementById("description-footer").style.display = "none";
     return;
   }
   const modalDescription = description.cloneNode(true) as HTMLElement;
   modalDescription.removeAttribute("id");
   modalDescription.removeAttribute("class");
-  document.getElementById(
-    "description-modal-content").append(modalDescription);
+  document.getElementById("description-modal-content").append(modalDescription);
 });
