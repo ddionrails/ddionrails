6872eb0bf16b589e0d270b0ab6e6667c1a1156f5
root <dhansen@diw.de>
2022-08-24 07:21:54 +0000

    Format feedback.ts

--- assets/js/feedback.ts
+++ assets/js/feedback.ts
@@ -1,26 +1,22 @@
 /** Handle referrer search url and switch email requirement status. */
 
-const email = document.getElementById("email")
+const email = document.getElementById("email");
 
 document.addEventListener("DOMContentLoaded", () => {
-	const source = document.getElementById("source") as HTMLInputElement
-	if (document.referrer.includes(document.location.href)) {
-		document.getElementById("feedback-success").classList.remove("hidden")
-		return
-	}
-	source.value = decodeURI(document.referrer)
-})
+  const source = document.getElementById("source") as HTMLInputElement;
+  if (document.referrer.includes(document.location.href)) {
+    document.getElementById("feedback-success").classList.remove("hidden");
+    return;
+  }
+  source.value = decodeURI(document.referrer);
+});
 
-document.getElementById("anon-submit-button").addEventListener("click",
-	(event) => {
-		email.removeAttribute("required")
-	}
+document
+  .getElementById("anon-submit-button")
+  .addEventListener("click", (event) => {
+    email.removeAttribute("required");
+  });
 
-)
-
-document.getElementById("submit-button").addEventListener("click",
-	(event) => {
-		email.setAttribute("required", "")
-	}
-
-)
+document.getElementById("submit-button").addEventListener("click", (event) => {
+  email.setAttribute("required", "");
+});
