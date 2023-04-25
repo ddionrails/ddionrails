b313fb365ed8ea3a4da24ed2fd89c5c1659200ee
root <dhansen@diw.de>
2023-04-25 12:39:33 +0000

    Transport commit

---
+++ assets/js/language_management.ts
@@ -0,0 +1,46 @@
+/**
+ *
+ * @returns
+ */
+function languageCode() {
+  const language = document.head
+    .querySelector("meta[name='language']")
+    .getAttribute("content");
+  if (language) {
+    return language;
+  }
+  return "en";
+}
+
+/**
+ *
+ * @returns
+ */
+function languageConfig() {
+  const language = languageCode();
+  if (language == "de") {
+    return {
+      lengthMenu: "Zeige _MENU_ Einträge pro Seite",
+      emptyTable: "Tabelle ist leer",
+      search: "Suche:",
+      zeroRecords: "Keine Suchergebnisse",
+      info: "Zeige Seite _PAGE_ von _PAGES_",
+      infoEmpty: "Keine Einträge vorhanden",
+      infoFiltered: "(Auswahl von insgesamt _MAX_ Einträgen)",
+      searchPlaceholder: "Alle Spalten durchsuchen",
+      paginate: {
+        first: "Anfang",
+        last: "Ende",
+        next: "Weiter",
+        previous: "Zurück",
+      },
+    };
+  }
+
+  return {
+    searchPlaceholder: "Search all columns",
+  };
+}
+
+export {languageCode};
+export {languageConfig};
