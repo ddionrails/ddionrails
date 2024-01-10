1c2ef41d101a29949adb7d193ebc1feca9a5b84c
root <dhansen@diw.de>
2024-01-10 13:42:52 +0000

    Add basic search ui connector

--- assets/js/search/main.ts
+++ assets/js/search/main.ts
@@ -1,1 +1,9 @@
+import ElasticsearchAPIConnector from "@elastic/search-ui-elasticsearch-connector";
+
 document.getElementById("app").innerHTML = "SEARCH";
+
+
+const connector = new ElasticsearchAPIConnector({
+  host: window.location.hostname + "/elastic/",
+  index: "variables",
+});
