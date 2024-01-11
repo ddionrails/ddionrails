fa7696962d811b1723163a4224bf53e3e29200ba
root <dhansen@diw.de>
2024-01-11 14:54:23 +0000

    Create minimal working react integration

--- assets/js/search/main.ts
+++ assets/js/search/main.ts
@@ -1,9 +1,13 @@
 import ElasticsearchAPIConnector from "@elastic/search-ui-elasticsearch-connector";
-
-document.getElementById("app").innerHTML = "SEARCH";
-
+import {createElement} from "react";
+import ReactDOM from "react-dom";
 
 const connector = new ElasticsearchAPIConnector({
   host: window.location.hostname + "/elastic/",
   index: "variables",
 });
+
+ReactDOM.render(
+  createElement("h1", {className: "HEADER"}, "SEARCH"),
+  document.getElementById("app")
+);
