fe652dd13a2306ff282684b9ec18c1143788fe81
root <dhansen@diw.de>
2024-01-22 13:35:25 +0000

    Add period to results

--- assets/js/search/view_customisations/variable_result_view.tsx
+++ assets/js/search/view_customisations/variable_result_view.tsx
@@ -16,26 +16,25 @@ export function VariableResultView({
     <li className="sui-result">
       <div className="sui-result__header">
         <h3>
-          {/* Maintain onClickLink to correct track click throughs for analytics*/}
           <i className="fa fa-cog"></i>
           <a onClick={onClickLink} href={"/variable/" + result._meta.id}>
-            {result.name.raw}
+            [{result.name.raw}] {result.label.raw}
           </a>
         </h3>
       </div>
       <div className="sui-result__body">
-        {/* use 'raw' values of fields to access values without snippets */}
         <p>
-            Study: {result.study.raw.name} | Dataset: {result.dataset.raw.name}
+          Period: {result.period.raw.label} | Study: {result.study.raw.name} |
+          Dataset: {result.dataset.raw.name}
         </p>
         <div className="sui-result__image">
           <img src={""} alt="" />
         </div>
-        {/* Use the 'snippet' property of fields with dangerouslySetInnerHtml to render snippets */}
         <div
           className="sui-result__details"
           dangerouslySetInnerHTML={{__html: result.description}}
         ></div>
       </div>
-    </li>);
-};
+    </li>
+  );
+}
