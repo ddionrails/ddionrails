5dfb6ecece3d395cd647de77a69b2177dc8a819f
root <dhansen@diw.de>
2024-02-06 13:42:49 +0000

    Modularize results

--- assets/js/search/view_customisations/variable_result_view.tsx
+++ assets/js/search/view_customisations/variable_result_view.tsx
@@ -1,6 +1,47 @@
 import {SearchResult} from "@elastic/search-ui";
 
 /**
+ * Render header for variable result
+ * @param result
+ * @returns
+ */
+function variableHeader(result: SearchResult, onClickLink: () => void) {
+  return (
+    <h3>
+      <i className="fa fa-cog"></i>
+      <a onClick={onClickLink} href={"/variable/" + result._meta.id}>
+            [{result.name.raw}]{" "}
+        <span
+          dangerouslySetInnerHTML={{__html: result.label.snippet}}
+        ></span>
+      </a>
+    </h3>
+
+  );
+}
+
+/**
+ * Render variable result body
+ * @param result
+ * @returns
+ */
+function variableBody(result: SearchResult) {
+  return (
+    <p>
+          Study: {result.study.raw.name} | Dataset: {result.dataset.raw.name}
+          | Period: {result.period.raw.label}
+    </p>
+  );
+}
+
+const mapDisplayFunctionsToIndex = (result: SearchResult) => {
+  if (result._meta.rawHit._index === "variables") {
+    return ({header: variableHeader, body: variableBody});
+  }
+  return (null);
+};
+
+/**
  *
  * @param param0
  * @returns
@@ -12,24 +53,14 @@ export function VariableResultView({
   result: SearchResult;
   onClickLink: () => void;
 }) {
+  const displayFunctions = mapDisplayFunctionsToIndex(result);
   return (
     <li className="sui-result">
       <div className="sui-result__header">
-        <h3>
-          <i className="fa fa-cog"></i>
-          <a onClick={onClickLink} href={"/variable/" + result._meta.id}>
-            [{result.name.raw}]{" "}
-            <span
-              dangerouslySetInnerHTML={{__html: result.label.snippet}}
-            ></span>
-          </a>
-        </h3>
+        {displayFunctions.header(result, onClickLink)}
       </div>
       <div className="sui-result__body">
-        <p>
-          Study: {result.study.raw.name} | Dataset: {result.dataset.raw.name}
-          | Period: {result.period.raw.label}
-        </p>
+        {displayFunctions.body(result)}
         <div className="sui-result__image">
           <img src={""} alt="" />
         </div>
