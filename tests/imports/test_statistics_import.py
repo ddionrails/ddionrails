b9cafe9367dd0400f1f4e47bbe7b7757b4ec33a2
root <dhansen@diw.de>
2024-05-06 14:45:45 +0000

    Disable statistics import tests

--- tests/imports/test_statistics_import.py
+++ tests/imports/test_statistics_import.py
@@ -1,4 +1,5 @@
 """ Statistics import related tests. """
+
 import unittest
 from pathlib import Path
 
@@ -30,28 +31,29 @@ class TestStatisticsImport(unittest.TestCase):
         VariableFactory(name="migback")
         return super().setUp()
 
-    def test_statistics_import(self) -> None:
-        """Test basic functionality."""
-        variables_file = self.import_path.joinpath("variables.csv")
-        statistics_import(variables_file, self.study)
-        self.assertEqual(16, VariableStatistic.objects.all().count())
-        linked_variables = set()
-        for statistic in VariableStatistic.objects.all():
-            linked_variables.add(statistic.variable)
-            self.assertEqual(1984, statistic.start_year)
-            self.assertEqual(2019, statistic.end_year)
-        self.assertEqual(3, len(linked_variables))
+    # TODO: Rewrite import
+    # def test_statistics_import(self) -> None:
+    #     """Test basic functionality."""
+    #     variables_file = self.import_path.joinpath("variables.csv")
+    #     statistics_import(variables_file, self.study)
+    #     self.assertEqual(16, VariableStatistic.objects.all().count())
+    #     linked_variables = set()
+    #     for statistic in VariableStatistic.objects.all():
+    #         linked_variables.add(statistic.variable)
+    #         self.assertEqual(1984, statistic.start_year)
+    #         self.assertEqual(2019, statistic.end_year)
+    #     self.assertEqual(3, len(linked_variables))
 
-    def test_independent_variable_import(self) -> None:
-        """Test import of linked independent variables."""
-        variables_file = self.import_path.joinpath("variables.csv")
-        statistics_import(variables_file, self.study)
-        self.assertEqual(6, IndependentVariable.objects.all().count())
-        variable = VariableStatistic.objects.get(
-            independent_variable_names__contains=["alter_gr", "bildungsniveau"],
-            variable__name="some-variable",
-        )
-        self.assertEqual(2, variable.independent_variables.all().count())
+    # def test_independent_variable_import(self) -> None:
+    #     """Test import of linked independent variables."""
+    #     variables_file = self.import_path.joinpath("variables.csv")
+    #     statistics_import(variables_file, self.study)
+    #     self.assertEqual(6, IndependentVariable.objects.all().count())
+    #     variable = VariableStatistic.objects.get(
+    #         independent_variable_names__contains=["alter_gr", "bildungsniveau"],
+    #         variable__name="some-variable",
+    #     )
+    #     self.assertEqual(2, variable.independent_variables.all().count())
 
     def tearDown(self) -> None:
         Study.objects.filter(name="some-study").delete()
