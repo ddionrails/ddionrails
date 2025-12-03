93efa70c925db0ac5fb63728903e1be5be932c7e
Dominique Seraphin Hansen <dhansen@diw.de>
2025-12-03 15:00:31 +0100

    Add basic setup and api request

--- assets/js/variable_relations.ts
+++ assets/js/variable_relations.ts
@@ -1,1 +1,44 @@
- F
+function getApiUrl(){
+
+  const variableMeta = document.querySelector('meta[name="variable"]');
+  const variableName = variableMeta instanceof HTMLMetaElement
+    ? variableMeta.content
+    : "";
+  const datasetMeta = document.querySelector('meta[name="dataset"]');
+  const datasetName = datasetMeta instanceof HTMLMetaElement
+    ? datasetMeta.content
+    : "";
+
+  if ( variableName == "" || datasetName == ""){
+    return "";
+  }
+
+  return `${window.location.origin}/api/related_variables/?dataset=${datasetName}&variable=${variableName}`
+
+
+}
+
+function loadRelationData(){
+  const apiUrl = getApiUrl()
+  if (apiUrl == ""){
+    return;
+  }
+
+  let apiRequest = new Request(apiUrl)
+  fetch(apiRequest).then(
+    (response) => {
+      response.json().then(
+        (json)=>{
+      console.log(json);
+      })
+    }
+  )
+
+
+
+}
+
+window.addEventListener("load", ()=>{
+  loadRelationData()
+
+})
