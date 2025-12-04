ecc44e588b061aa3c84ca73f4f7879adb52631b4
Dominique Seraphin Hansen <dhansen@diw.de>
2025-12-04 15:11:12 +0100

    Add basic long variable nodes to info box

--- assets/js/variable_relations.ts
+++ assets/js/variable_relations.ts
@@ -1,44 +1,69 @@
-function getApiUrl(){
+const longVariableConainerID = "long-variable-info-container";
 
+const outputIcon = document.createElement("i")
+outputIcon.classList.add("fa-solid", "fa-arrow-right-from-bracket", "output-icon")
+
+const inputIcon = document.createElement("i")
+inputIcon.classList.add("fa-solid", "fa-arrow-right-to-bracket", "input-icon")
+
+function getApiUrl() {
+  const studyMeta = document.querySelector('meta[name="study"]');
   const variableMeta = document.querySelector('meta[name="variable"]');
-  const variableName = variableMeta instanceof HTMLMetaElement
-    ? variableMeta.content
-    : "";
+  const variableName =
+    variableMeta instanceof HTMLMetaElement ? variableMeta.content : "";
   const datasetMeta = document.querySelector('meta[name="dataset"]');
-  const datasetName = datasetMeta instanceof HTMLMetaElement
-    ? datasetMeta.content
-    : "";
+  const datasetName =
+    datasetMeta instanceof HTMLMetaElement ? datasetMeta.content : "";
+  const studyName =
+    studyMeta instanceof HTMLMetaElement ? studyMeta.content : "";
 
-  if ( variableName == "" || datasetName == ""){
+  if (variableName == "" || datasetName == "" || studyName == "") {
     return "";
   }
+  const query = `?dataset=${datasetName}&variable=${variableName}&study=${studyName}`;
 
-  return `${window.location.origin}/api/related_variables/?dataset=${datasetName}&variable=${variableName}`
-
-
+  return `${window.location.origin}/api/related_variables/${query}`;
 }
 
-function loadRelationData(){
-  const apiUrl = getApiUrl()
-  if (apiUrl == ""){
+function parseRelatedJSON(json: any) {
+  const content = json;
+  if (!content?.["count"]) {
+    return;
+  }
+  const longVariables = [];
+  for (const result of content?.["results"]) {
+    if (result?.["period_name"] == "0") {
+      longVariables.push(result);
+    }
+  }
+  const container = document.getElementById(longVariableConainerID);
+  if(longVariables.length == 1){
+    container.appendChild(document.createTextNode("Long Variable: "))
+  } else if (longVariables.length > 1){
+    container.appendChild(document.createTextNode("Harmonized from: "))
+  }
+  for (const variable of longVariables) {
+    let element = document.createElement("div");
+    element.appendChild(document.createTextNode(variable?.["name"]));
+    element.appendChild(inputIcon.cloneNode())
+    container.appendChild(element);
+  }
+}
+
+function loadRelationData() {
+  const apiUrl = getApiUrl();
+  if (apiUrl == "") {
     return;
   }
 
-  let apiRequest = new Request(apiUrl)
-  fetch(apiRequest).then(
-    (response) => {
-      response.json().then(
-        (json)=>{
-      console.log(json);
-      })
-    }
-  )
-
-
-
+  let apiRequest = new Request(apiUrl);
+  fetch(apiRequest).then((response) => {
+    response.json().then((json) => {
+      parseRelatedJSON(json);
+    });
+  });
 }
 
-window.addEventListener("load", ()=>{
-  loadRelationData()
-
-})
+window.addEventListener("load", () => {
+  loadRelationData();
+});
