44dd4817d9e3e838edbc6f308d03e432f181548a
Dominique Seraphin Hansen <dhansen@diw.de>
2026-01-07 15:19:40 +0100

    Add concept icons in variable relations

---
+++ assets/js/variable_relations_concepts.ts
@@ -0,0 +1,78 @@
+import { createElement } from "react";
+
+type Variable = {
+  id: string;
+  name: string;
+  period_name: string;
+};
+
+type VariableList = Array<Variable>;
+
+export function createIcon(
+  classNames: Array<string>,
+  languageLabels: Map<languageCode, string>,
+): HTMLElement {
+  const languageSwitch = document.getElementById("language-switch");
+  let language = languageSwitch.getAttribute(
+    "data-current-language",
+  ) as languageCode;
+
+  const inputIcon = document.createElement("i");
+  inputIcon.classList.add(...classNames);
+  inputIcon.title = languageLabels.get(language);
+
+  const mutationCallback = (mutationList: Array<any>, _: any) => {
+    for (const mutation of mutationList) {
+      if (mutation.type == "attributes") {
+        language = languageSwitch.getAttribute(
+          "data-current-language",
+        ) as languageCode;
+        inputIcon.title = languageLabels.get(language);
+      }
+    }
+  };
+  const observer = new MutationObserver(mutationCallback);
+  observer.observe(languageSwitch, { attributes: true });
+
+  return inputIcon;
+}
+
+function getAPIURL() {
+  const studyMeta = document.querySelector('meta[name="study"]');
+  const studyName =
+    studyMeta instanceof HTMLMetaElement ? studyMeta.content : "";
+
+  const conceptMeta = document.querySelector('meta[name="concept"]');
+  const conceptName =
+    conceptMeta instanceof HTMLMetaElement ? conceptMeta.content : "";
+  if (conceptName == "") {
+    return "";
+  }
+
+  const query = `?study=${studyName}&concept=${conceptName}`;
+  return `${window.location.origin}/api/variables/${query}`;
+}
+
+export async function addConceptToVariables() {
+  const apiURL = getAPIURL();
+
+  fetch(apiURL).then((response) => {
+    response.json().then((json: VariableList) => {
+      for (const variable of json) {
+        const variableContainer = document.querySelector(
+          `div[data-period-name='${variable.period_name}']` +
+            ` > div[data-variable-name='${variable.name}']`,
+        );
+        const conceptIcon = createIcon(
+          ["fa", "fa-cog", "concept-icon"],
+          new Map([
+            ["en", "Is part of the same concept"],
+            ["de", "Gehört zum selben Konzept"],
+          ]),
+        );
+        variableContainer.appendChild(conceptIcon)
+      }
+    });
+  });
+}
+
