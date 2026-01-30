dec738d510979a08ec3d130965ebe4a6bceee43c
Dominique Seraphin Hansen <dhansen@diw.de>
2026-01-30 15:06:11 +0100

    Start reworking test factories

--- tests/factories.py
+++ tests/factories.py
@@ -1,19 +1,55 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring,too-few-public-methods
-
+# type: ignore[override]
 """DjangoModelFactories for user model"""
+
 
 import factory
 from django.contrib.auth.models import User
+from factory.django import DjangoModelFactory
+from faker import Faker
+
+from ddionrails.concepts.models import Period
+from ddionrails.studies.models import Study
+
+FAKE = Faker()
 
 
-class UserFactory(factory.django.DjangoModelFactory):
+def german_paragraph(_):
+    with factory.Faker.override_default_locale("de_DE"):
+        paragraphs = FAKE.paragraphs()
+    return paragraphs
+
+
+def german_label(_):
+    with factory.Faker.override_default_locale("de_DE"):
+        word = FAKE.word()
+    return word
+
+
+class UserFactory(DjangoModelFactory):
     class Meta:
         model = User
         django_get_or_create = ("username",)
 
     username = "test_user"
     password = factory.PostGenerationMethodCall("set_password", "secret-password")
+
+
+class StudyFactory(DjangoModelFactory):
+    class Meta:
+        model = Study
+
+    name = factory.LazyAttribute(lambda _: FAKE.word())
+    label = factory.LazyAttribute(lambda _: FAKE.word())
+    label_de = factory.LazyAttribute(german_label)
+    description = factory.LazyAttribute(lambda _: FAKE.paragraphs())
+    description_de = factory.LazyAttribute(german_paragraph)
+
+
+class PeriodFactory(DjangoModelFactory):
+    class Meta:
+        model = Period
 
 
 # TODO: Rework this with all general factories
