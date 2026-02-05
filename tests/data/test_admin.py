10ff07ff0e65525e0662df59050bb70e41ac34c0
Dominique Seraphin Hansen <dhansen@diw.de>
2026-02-05 14:33:20 +0100

    Start replacing old test factories

--- tests/data/test_admin.py
+++ tests/data/test_admin.py
@@ -1,59 +1,68 @@
 # -*- coding: utf-8 -*-
-""" Test cases for ModelAdmins in ddionrails.data app """
+"""Test cases for ModelAdmins in ddionrails.data app"""
 
-import pytest
+
+from django.test import Client, TestCase
+from django.test.utils import override_settings
 from django.urls import reverse
 
 from tests import status
-
-# One of the admin views in django itself
-# uses {% load staticfiles %} instead {% load static %}.
-# It throws a "django.utils.deprecation.RemovedInDjango30Warning"
-pytestmark = [  # pylint: disable=invalid-name
-    pytest.mark.django_db,
-    pytest.mark.filterwarnings("ignore::DeprecationWarning"),
-]
+from tests.factories import AdminUserFactory, TransformationFactory, VariableFactory
 
 
-def test_dataset_admin_list(admin_client, dataset):  # pylint: disable=unused-argument
-    """ Test the DatasetAdmin changelist """
-    url = reverse("admin:data_dataset_changelist")
-    response = admin_client.get(url)
-    assert status.HTTP_200_OK == response.status_code
+class TestAdmin(TestCase):
+    """Test admin views"""
 
+    admin_client: Client
+    variable: VariableFactory
 
-def test_dataset_admin_detail(admin_client, dataset):
-    """ Test the DatasetAdmin change_view """
-    url = reverse("admin:data_dataset_change", args=(dataset.id,))
-    response = admin_client.get(url)
-    assert status.HTTP_200_OK == response.status_code
+    def setUp(self) -> None:
+        admin_password = "123test"  # nosec
+        admin_user = AdminUserFactory(password=admin_password)
+        self.admin_client = Client()
+        self.admin_client.login(username=admin_user.username, password=admin_password)
+        self.variable = VariableFactory()
+        self._override_settings = override_settings(
+            DEBUG_TOOLBAR_CONFIG={"SHOW_TOOLBAR_CALLBACK": lambda request: False},
+        )
+        self._override_settings.enable()
 
+        return super().setUp()
 
-def test_variable_admin_list(admin_client, variable):  # pylint: disable=unused-argument
-    """ Test the VariableAdmin changelist """
-    url = reverse("admin:data_variable_changelist")
-    response = admin_client.get(url)
-    assert status.HTTP_200_OK == response.status_code
+    def test_dataset_admin_list(self):
+        """Test the DatasetAdmin changelist"""
 
+        url = reverse("admin:data_dataset_change", args=(str(self.variable.dataset.id),))
 
-def test_variable_admin_detail(admin_client, variable):
-    """ Test the VariableAdmin change_view """
-    url = reverse("admin:data_variable_change", args=(variable.id,))
-    response = admin_client.get(url)
-    assert status.HTTP_200_OK == response.status_code
+        response = self.admin_client.get(url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
 
+    def test_variable_admin_list(self):
+        """Test the VariableAdmin changelist"""
+        url = reverse("admin:data_variable_changelist")
+        response = self.admin_client.get(url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
 
-def test_transformation_admin_list(
-    admin_client, transformation
-):  # pylint: disable=unused-argument
-    """ Test the TransformationAdmin changelist """
-    url = reverse("admin:data_transformation_changelist")
-    response = admin_client.get(url)
-    assert status.HTTP_200_OK == response.status_code
+    def test_variable_admin_detail(self):
+        """Test the VariableAdmin change_view"""
+        url = reverse("admin:data_variable_change", args=(str(self.variable.id),))
+        response = self.admin_client.get(url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
 
+    def test_transformation_admin_list(self):  # pylint: disable=unused-argument
+        """Test the TransformationAdmin changelist"""
+        _ = TransformationFactory(origin=self.variable)
+        url = reverse("admin:data_transformation_changelist")
+        response = self.admin_client.get(url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
 
-def test_transformation_admin_detail(admin_client, transformation):
-    """ Test the TransformationAdmin change_view """
-    url = reverse("admin:data_transformation_change", args=(transformation.id,))
-    response = admin_client.get(url)
-    assert status.HTTP_200_OK == response.status_code
+    def test_transformation_admin_detail(self):
+        """Test the TransformationAdmin change_view"""
+        transformation = TransformationFactory(origin=self.variable)
+        url = reverse("admin:data_transformation_change", args=(str(transformation.id),))
+        response = self.admin_client.get(url)
+        assert status.HTTP_200_OK == response.status_code
+
+    def tearDown(self) -> None:
+        self._override_settings.disable()
+        return super().tearDown()
