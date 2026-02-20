bb8a60aeea27105f2cef64b0bb2b650aba2726d5
Dominique Seraphin Hansen <dhansen@diw.de>
2026-02-20 16:01:04 +0100

    Fix config error

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,7 +81,6 @@ DJANGO_APPS = (
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.contrib.postgres",
 )
 THIRD_PARTY_APPS = (
     "crispy_forms",
