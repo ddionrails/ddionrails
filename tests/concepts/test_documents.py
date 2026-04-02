3fd385a19743bd42765026eb279210c35f8aae7c
Dominique Seraphin Hansen <dhansen@diw.de>
2026-04-02 12:03:02 +0200

    Rework concept tests

--- tests/concepts/test_documents.py
+++ tests/concepts/test_documents.py
@@ -3,38 +3,34 @@
 
 """Test cases for documents in ddionrails.concepts app"""
 
-from os import getenv
 from typing import Any
-from unittest import TestCase
 
-import pytest
 from django.forms.models import model_to_dict
 from django.test import LiveServerTestCase, override_settings
 
 from ddionrails.concepts.documents import ConceptDocument, TopicDocument
-from ddionrails.concepts.models import Concept
-from ddionrails.studies.models import Study
 from tests.functional.search_index_fixtures import set_up_index, tear_down_index
-
-INDEX_PREFIX = getenv("ELASTICSEARCH_DSL_INDEX_PREFIX", "")
-
-
-pytestmark = [
-    pytest.mark.search,
-    pytest.mark.filterwarnings("ignore::DeprecationWarning"),
-]
-
-TEST_CASE = TestCase()
+from tests.model_factories import (
+    ConceptFactory,
+    StudyFactory,
+    TopicFactory,
+    VariableFactory,
+)
 
 
 @override_settings(ROOT_URLCONF="tests.functional.browser.search.mock_urls")
-@pytest.mark.usefixtures("variable_with_concept", "topic", "concept")
 class TestDocuments(LiveServerTestCase):
     variable_with_concept: Any
     topic: Any
     concept: Any
 
     def setUp(self) -> None:
+        self.study = StudyFactory()
+        self.topic = TopicFactory(study=self.study)
+        self.concept = ConceptFactory(topics=[self.topic])
+        self.variable_with_concept = VariableFactory(
+            concept=self.concept, dataset__study=self.study
+        )
         set_up_index(self, self.concept, "concepts")
         set_up_index(self, self.topic, "topics")
         return super().setUp()
@@ -91,11 +87,15 @@ class TestDocuments(LiveServerTestCase):
         for key, value in expected.items():
             self.assertEqual(value, result[key], msg=f"Problem in {key}")
         for key in result.keys():
-            TEST_CASE.assertIn(key, expected)
+            self.assertIn(key, expected)
         assert expected == result
 
     def test_search_concept_by_label_de(self):
-        result = ConceptDocument.search().query("match", label_de="Konzept").count()
+        result = (
+            ConceptDocument.search()
+            .query("match", label_de=self.concept.label_de)
+            .count()
+        )
         expected = 1
         assert expected == result
 
@@ -118,11 +118,12 @@ class TestDocuments(LiveServerTestCase):
         )
         # add relations to expected dictionary
         expected["study_name"] = self.topic.study.title()
-        expected["study_name_de"] = self.topic.study.label
+        expected["study_name_de"] = self.topic.study.label_de
         expected["id"] = str(self.topic.id)
         expected["study"] = {
             "name": self.topic.study.name,
             "label": self.topic.study.label,
+            "label_de": self.topic.study.label_de,
         }
 
         # generate result dictionary from search document
